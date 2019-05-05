d6ea34f53adf87190d6f51876a8751989b3d51bf
love1 <love1dbsb77@hotmail.com>
2019-05-05 16:21:34 -0700

    Added missing component import and result component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import { Search } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import 'semantic-ui-css/semantic.min.css';
+
+import SearchBar from './components/SearchBar';
 
 const SearchBarContainer = (props) => {
     return (
         <SearchBar
             items={props.items? props.items : []}
             onResultSelect={props.onResultSelect ? props.onResultSelect : (e, { value }) => { console.log(value); }}
-            customComponent={props.customComponent ? props.customComponent : Search.Result}
+            customComponent={props.customComponent ? props.customComponent : Result}
         />
+    )
+}
+
+const Result = (props) => {
+    const {image, title, price, description} = props;
+    return (
+            <div>
+                {image &&
+                <div key='image' className='image'>
+                    {createHTMLImage(image, { autoGenerateKey: false })}
+                </div>
+                }
+                <div key='content' className='content'>
+                {price && <div className='price'>{price}</div>}
+                {title && <div className='title'>{title}</div>}
+                {description && <div className='description'>{description}</div>}
+                </div>
+            </div>
     )
 }
 
